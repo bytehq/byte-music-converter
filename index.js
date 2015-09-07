dc01852f5e0885489593f7d85b4205eb418663fe
Dom Hofmann <sevenflow@gmail.com>
2015-09-07 19:26:03 -0400

    Initial file commit

---
+++ index.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+var music = {"name": "music 7F424A6C-65FA-4FA4-B5F4-BC0F8C615E05", "frame": [108, 399, 108, 114], "bpm": 80, "length": 16, "effects": [], "type": "music", "instructions": [[{"velo": 31, "note": "Crash", "time": 0, "duration": 1, "type": 1, "bank": "drums"}, {"velo": 62, "note": "Tom", "time": 0, "duration": 1, "type": 1, "bank": "drums"}], [], [], [], [{"velo": 41, "note": "E/3", "time": 1, "duration": 1, "type": 0, "bank": "bleep"}], [{"velo": 41, "note": "E/3", "time": 1.25, "duration": 1, "type": 0, "bank": "bleep"}], [{"velo": 41, "note": "G/3", "time": 1.5, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 31, "note": "E/3", "time": 1.5, "duration": 1, "type": 0, "bank": "bell"}], [{"velo": 31, "note": "G/3", "time": 1.75, "duration": 1, "type": 0, "bank": "bell"}, {"velo": 41, "note": "E/3", "time": 1.75, "duration": 1, "type": 0, "bank": "bleep"}], [{"velo": 41, "note": "B/3", "time": 2, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 52, "note": "G/3", "time": 2, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "E/3", "time": 2, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 114, "note": "Kick", "time": 2, "duration": 1, "type": 1, "bank": "drums"}, {"velo": 93, "note": "C/3", "time": 2, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 31, "note": "B/3", "time": 2.25, "duration": 1, "type": 0, "bank": "bell"}], [], [{"velo": 41, "note": "B/3", "time": 2.75, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 52, "note": "G/3", "time": 2.75, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "E/3", "time": 2.75, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 114, "note": "Kick", "time": 2.75, "duration": 1, "type": 1, "bank": "drums"}, {"velo": 93, "note": "C/3", "time": 2.75, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 31, "note": "B/3", "time": 3, "duration": 1, "type": 0, "bank": "bell"}], [{"velo": 52, "note": "D/3", "time": 3.4375, "duration": 1, "type": 0, "bank": "ping"}], [{"velo": 41, "note": "A/3", "time": 3.5, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 52, "note": "F#/3", "time": 3.5, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 93, "note": "D/3", "time": 3.5, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 114, "note": "Kick", "time": 3.5, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 31, "note": "A/3", "time": 3.75, "duration": 1, "type": 0, "bank": "bell"}], [], [], [], [], [{"velo": 41, "note": "E/3", "time": 5, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 62, "note": "Glitch", "time": 5, "duration": 1, "type": 1, "bank": "drums"}, {"velo": 83, "note": "Clap", "time": 5, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 41, "note": "E/3", "time": 5.25, "duration": 1, "type": 0, "bank": "mono1"}, {"velo": 114, "note": "Kick", "time": 5.25, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 41, "note": "G/3", "time": 5.5, "duration": 1, "type": 0, "bank": "mono1"}, {"velo": 31, "note": "E/3", "time": 5.5, "duration": 1, "type": 0, "bank": "bell"}, {"velo": 93, "note": "D/3", "time": 5.5, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 31, "note": "G/3", "time": 5.75, "duration": 1, "type": 0, "bank": "bell"}, {"velo": 41, "note": "E/3", "time": 5.75, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 93, "note": "D/3", "time": 5.75, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 93, "note": "B/3", "time": 6, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 41, "note": "A/3", "time": 6, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 52, "note": "F#/3", "time": 6, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "D/3", "time": 6, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 114, "note": "Kick", "time": 6, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 31, "note": "A/3", "time": 6.25, "duration": 1, "type": 0, "bank": "bell"}], [], [{"velo": 93, "note": "B/3", "time": 6.75, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 41, "note": "A/3", "time": 6.75, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 52, "note": "F#/3", "time": 6.75, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "D/3", "time": 6.75, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 114, "note": "Kick", "time": 6.75, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 31, "note": "A/3", "time": 7, "duration": 1, "type": 0, "bank": "bell"}], [{"velo": 52, "note": "G/3", "time": 7.4375, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "E/3", "time": 7.4375, "duration": 1, "type": 0, "bank": "ping"}], [{"velo": 41, "note": "G/3", "time": 7.5, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 93, "note": "E/3", "time": 7.5, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 114, "note": "Kick", "time": 7.5, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 31, "note": "G/3", "time": 7.75, "duration": 1, "type": 0, "bank": "bell"}], [], [], [{"velo": 93, "note": "E/3", "time": 8.5, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 93, "note": "F#/3", "time": 8.75, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 93, "note": "G/3", "time": 9, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 83, "note": "Clap", "time": 9, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 93, "note": "F#/3", "time": 9.25, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 41, "note": "E/3", "time": 9.5, "duration": 1, "type": 0, "bank": "bleep"}], [{"velo": 52, "note": "G/3", "time": 9.9375, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 31, "note": "E/3", "time": 9.75, "duration": 1, "type": 0, "bank": "bell"}, {"velo": 93, "note": "D/3", "time": 9.75, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 114, "note": "Kick", "time": 9.75, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 41, "note": "G/3", "time": 10, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 52, "note": "E/3", "time": 10, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 114, "note": "Kick", "time": 10, "duration": 1, "type": 1, "bank": "drums"}, {"velo": 93, "note": "C/3", "time": 10, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 31, "note": "G/3", "time": 10.25, "duration": 1, "type": 0, "bank": "bell"}], [], [{"velo": 52, "note": "G/3", "time": 10.75, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "E/3", "time": 10.75, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 114, "note": "Kick", "time": 10.75, "duration": 1, "type": 1, "bank": "drums"}, {"velo": 93, "note": "C/3", "time": 10.75, "duration": 1, "type": 0, "bank": "funk"}], [{"velo": 41, "note": "A/3", "time": 11, "duration": 1, "type": 0, "bank": "bleep"}], [{"velo": 31, "note": "A/3", "time": 11.25, "duration": 1, "type": 0, "bank": "bell"}, {"velo": 52, "note": "F#/3", "time": 11.4375, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "D/3", "time": 11.4375, "duration": 1, "type": 0, "bank": "ping"}], [{"velo": 41, "note": "F#/3", "time": 11.5, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 93, "note": "D/3", "time": 11.5, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 114, "note": "Kick", "time": 11.5, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 31, "note": "F#/3", "time": 11.75, "duration": 1, "type": 0, "bank": "bell"}], [], [{"velo": 41, "note": "E/3", "time": 12.25, "duration": 1, "type": 0, "bank": "mono1"}], [{"velo": 41, "note": "D/3", "time": 12.5, "duration": 1, "type": 0, "bank": "bleep"}], [{"velo": 31, "note": "D/3", "time": 12.75, "duration": 1, "type": 0, "bank": "bell"}], [{"velo": 62, "note": "Glitch", "time": 13, "duration": 1, "type": 1, "bank": "drums"}, {"velo": 83, "note": "Cowbell", "time": 13, "duration": 1, "type": 1, "bank": "drums"}, {"velo": 83, "note": "Clap", "time": 13, "duration": 1, "type": 1, "bank": "drums"}], [], [{"velo": 41, "note": "D/3", "time": 13.5, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 114, "note": "Kick", "time": 13.5, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 31, "note": "D/3", "time": 13.75, "duration": 1, "type": 0, "bank": "bell"}], [{"velo": 93, "note": "B/3", "time": 14, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 41, "note": "A/3", "time": 14, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 52, "note": "F#/3", "time": 14, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "D/3", "time": 14, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 114, "note": "Kick", "time": 14, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 31, "note": "A/3", "time": 14.25, "duration": 1, "type": 0, "bank": "bell"}], [], [{"velo": 52, "note": "G/3", "time": 14.9375, "duration": 1, "type": 0, "bank": "ping"}, {"velo": 52, "note": "E/3", "time": 14.9375, "duration": 1, "type": 0, "bank": "ping"}], [{"velo": 41, "note": "G/3", "time": 15, "duration": 1, "type": 0, "bank": "bleep"}, {"velo": 93, "note": "E/3", "time": 15, "duration": 1, "type": 0, "bank": "funk"}, {"velo": 114, "note": "Kick", "time": 15, "duration": 1, "type": 1, "bank": "drums"}], [{"velo": 31, "note": "G/3", "time": 15.25, "duration": 1, "type": 0, "bank": "bell"}], [{"velo": 62, "note": "G/3", "time": 15.5, "duration": 1, "type": 0, "bank": "bark"}], [{"velo": 62, "note": "G/3", "time": 15.75, "duration": 1, "type": 0, "bank": "bark"}], []]};
+
+var fs = require('fs');
+var midi = require('jsmidgen');
+var exec = require('child_process').exec;
+
+console.log('BFF Music -> MIDI...');
+
+var file = new midi.File();
+var tracks = ["bleep", "meow", "bass", "ping", "string", "reso", "arp", "bark", "mono1", "mono2", "mono3", "funk", "sax", "bell", "roboto", /*"do",*/ "drums"];
+var midiTracks = [];
+
+var notes = ["A/-1", "A#/-1", "B/-1", "C/0", "C#/0", "D/0", "D#/0", "E/0", "F/0", "F#/0", "G/0", "G#/0",
+            "A/0", "A#/0", "B/0", "C/1", "C#/1", "D/1", "D#/1", "E/1", "F/1", "F#/1", "G/1", "G#/1",
+            "A/1", "A#/1", "B/1", "C/2", "C#/2", "D/2", "D#/2", "E/2", "F/2", "F#/2", "G/2", "G#/2",
+            "A/2", "A#/2", "B/2", "C/3", "C#/3", "D/3", "D#/3", "E/3", "F/3", "F#/3", "G/3", "G#/3",
+            "A/3", "A#/3", "B/3", "C/4", "C#/4", "D/4", "D#/4", "E/4", "F/4", "F#/4", "G/4", "G#/4",
+            "A/4", "A#/4", "B/4", "C/5", "C#/5", "D/5", "D#/5", "E/5", "F/5", "F#/5", "G/5", "G#/5",
+            "A/5", "A#/5", "B/5", "C/6", "C#/6", "D/6", "D#/6", "E/6", "F/6", "F#/6", "G/6", "G#/6",
+            "A/6", "A#/6", "B/6", "C/7", "C#/7", "D/7", "D#/7", "E/7", "F/7", "F#/7", "G/7", "G#/7",
+            "A/7", "A#/7", "B/7", "C/8", "C#/8", "D/8", "D#/8", "E/8", "F/8", "F#/8", "G/8"];
+var drums = ["Kick", "Snare", "Clap", "Hat", "Thump", "Glitch", "Tambourine", "Whistle", "Block", "Stick", "Shaker", "Crash", "Tom", "Conga", "Cowbell", "Yeah"];
+
+var trackIndex = function (trackName) {
+    return tracks.indexOf(trackName);
+}
+
+var bankIndex = function (bankName) {
+    if (bankName == 'drums') {
+        return 127;
+    }
+
+    return tracks.indexOf(bankName);
+};
+
+var note = function (note) {
+    return notes.indexOf(note) + 21;
+};
+
+var drumNote = function (drum) {
+    return drums.indexOf(drum) + 36;
+};
+
+for (var i = 0; i < tracks.length; i++) {
+    var track = file.addTrack();
+    midiTracks.push(track);
+}
+
+for (var i = 0; i < tracks.length; i++) {
+    midiTracks[0].setInstrument(i, i == 15 ? 127 : i);
+    midiTracks[0].setTempo(music['bpm'], 0);
+}
+var instructions = [];
+music['instructions'].forEach(function (beat, index) {
+    beat.forEach(function (instruction, index) {
+        var offset = parseInt(128 * instruction['time'])
+        var track = midiTracks[trackIndex(instruction['bank'])];
+        var midiNote;
+        if (instruction.type == 0) {
+            midiNote = note(instruction['note']);
+        } else {
+            midiNote = drumNote(instruction['note']);
+        }
+        instructions.push({velocity: instruction['velo'], offset: offset, note: midiNote, track: trackIndex(instruction['bank'])});
+    });
+});
+
+instructions = instructions.sort(function (a, b) {
+    return a['offset'] - b['offset'];
+});
+
+var currentOffset = 0;
+instructions.forEach(function (instruction, index) {
+    var delay = 0;
+    if (instruction.offset > currentOffset) {
+        delay = instruction.offset - currentOffset;
+        currentOffset = instruction.offset;
+    }
+
+    midiTracks[0].addNoteOn(instruction.track, instruction.note, delay, instruction.velocity);
+});
+
+fs.writeFileSync('out.mid', file.toBytes(), 'binary');
+console.log('MIDI -> WAV...');
+exec('fluidsynth --gain 2.0 -F out.wav ./resources/Byte.sf2 out.mid', function(error, stdout, stderr) {
+    console.log('WAV -> MP3...');
+    exec('lame --preset standard out.wav out.mp3', function(error, stdout, stderr) {
+        console.log('All done');
+    });
+});
