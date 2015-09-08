bc37a0503d02eb8f67a1c7d8ae67f54756252c59
Jacob Marttinen <jacob@marttinen.ca>
2015-09-08 19:09:53 -0400

    More deploy fixes

--- gce/add-new-instance-template.sh
+++ gce/add-new-instance-template.sh
@@ -7,14 +7,15 @@ ZONE=us-central1-c
 APP_ID=byte-music-converter
 
 GROUP=$APP_ID
-TEMPLATE=$GROUP-tmpl-150908
+TEMPLATE=$GROUP-tmpl-150908-3
 MACHINE_TYPE=g1-small
 STARTUP_SCRIPT=startup-script.sh
-SCOPES="logging-write \
-storage-rw \
-https://www.googleapis.com/auth/projecthosting \
-https://www.googleapis.com/auth/cloud-platform \
-https://www.googleapis.com/auth/userinfo.email"
+SCOPES="userinfo-email,\
+logging-write,\
+storage-full,\
+datastore,\
+https://www.googleapis.com/auth/pubsub,\
+https://www.googleapis.com/auth/projecthosting"
 TAGS=http-server
 
 SERVICE=$APP_ID-service
@@ -30,7 +31,6 @@ SERVICE=$APP_ID-service
 gcloud compute instance-templates create $TEMPLATE \
   --machine-type $MACHINE_TYPE \
   --scopes $SCOPES \
-  --metadata DEPLOY_KEY=$DEPLOY_KEY,\
-  API_AUTH_KEY=$BYTE_MUSIC_CONVERTER_AUTH_KEY \
+  --metadata DEPLOY_KEY=$DEPLOY_KEY,API_AUTH_KEY=$BYTE_MUSIC_CONVERTER_AUTH_KEY \
   --metadata-from-file startup-script=$STARTUP_SCRIPT \
   --tags $TAGS
