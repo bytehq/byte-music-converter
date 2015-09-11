c1500f22a7bc12ff2b0b2f7379fcc3a13ea01552
Jacob Marttinen <jacob@marttinen.ca>
2015-09-11 19:48:32 -0400

    Adding second box to deploy script

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 curl -X GET -H "Accept:application/json" -H "Authorization: Bearer $DEPLOY_KEY" http://104.197.142.237:3000/_ah/deploy
+curl -X GET -H "Accept:application/json" -H "Authorization: Bearer $DEPLOY_KEY" http://104.154.68.92:3000/_ah/deploy
